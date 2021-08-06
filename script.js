b3db51c99782db79fa02cdf5afd695404c56b50f
aditiyanurwahid <aditiyanurwahid1995@gmail.com>
2021-08-07 05:37:21 +0700

    edit looping

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const allCountry = fetch("https://covid19.mathdro.id/api/confirmed")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    for (let i = 0; i <= 494; i++) {
+    for (let i = 0; i <= 200; i++) {
       document.getElementById("semuaNegara").innerHTML += `
       <section>
         <div class="container-fluid">
