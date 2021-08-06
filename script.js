01bcd9ced7c5fb503da03ae99e8a85a6ecc53b20
aditiyanurwahid <aditiyanurwahid1995@gmail.com>
2021-08-07 04:04:07 +0700

    edit index

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ const dataGlobal = fetch("https://covid19.mathdro.id/api")
       (document.getElementById("deaths-global").innerHTML = `Deaths: ${data.deaths.value}`)
   );
 
-// Fitur search sedang dalam proses pengerjaan
+// SEMUA NEGARA
+const semuaNegara = document.getElementById("semuaNegara");
+const allCountry = fetch("https://covid19.mathdro.id/api/confirmed")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((data) => {
+      semuaNegara.innerHTML += `
+      <section>
+      <div class="container-fluid">
+        <div class="row text-center">
+          <div class="fw-light fs-5 bg-dark bg-gradient text-white" id="countryRegion">${data.countryRegion}</div>
+          <div class="col bg-warning bg-gradient text-white" id="confirmrd">Confirmed: ${data.confirmed}</div>
+          <div class="col bg-success bg-gradient text-white" id="recovered">Recovered: ${data.recovered}</div>
+          <div class="col bg-danger bg-gradient text-white" id="deaths">Deaths: ${data.deaths}</div>
+        </div>
+      </div>
+      </section>
+      `;
+    });
+  });
