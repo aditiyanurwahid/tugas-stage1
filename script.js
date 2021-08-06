b621fc119695548635900f3e6d0b02615bd63c02
aditiyanurwahid <aditiyanurwahid1995@gmail.com>
2021-08-07 05:11:19 +0700

    edit looping

--- script.js
+++ script.js
@@ -17,22 +17,23 @@ const dataGlobal = fetch("https://covid19.mathdro.id/api")
   );
 
 // SEMUA NEGARA
-const semuaNegara = document.getElementById("semuaNegara");
+// const semuaNegara = document.getElementById("semuaNegara");
 const allCountry = fetch("https://covid19.mathdro.id/api/confirmed")
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((data) => {
-      semuaNegara.innerHTML += `
+    console.log(data);
+    for (let i = 0; i <= 3946; i++) {
+      document.getElementById("semuaNegara").innerHTML += `
       <section>
-      <div class="container-fluid">
-        <div class="row text-center">
-          <div class="fw-light fs-5 bg-dark bg-gradient text-white" id="countryRegion">${data.countryRegion}</div>
-          <div class="col bg-warning bg-gradient text-white" id="confirmrd">Confirmed: ${data.confirmed}</div>
-          <div class="col bg-success bg-gradient text-white" id="recovered">Recovered: ${data.recovered}</div>
-          <div class="col bg-danger bg-gradient text-white" id="deaths">Deaths: ${data.deaths}</div>
+        <div class="container-fluid">
+          <div class="row text-center">
+            <div class="fw-light fs-5 bg-dark bg-gradient text-white" id="countryRegion">${data[i].countryRegion}</div>
+            <div class="col bg-warning bg-gradient text-white" id="confirmrd">Confirmed: ${data[i].confirmed}</div>
+            <div class="col bg-success bg-gradient text-white" id="recovered">Recovered: ${data[i].recovered}</div>
+            <div class="col bg-danger bg-gradient text-white" id="deaths">Deaths: ${data[i].deaths}</div>
+          </div>
         </div>
-      </div>
       </section>
       `;
-    });
+    }
   });
